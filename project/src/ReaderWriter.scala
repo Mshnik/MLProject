202de4f397e4ac838308a22d72ce0c2cdf3b5505
Michael Patashnik <Mgpshnik@gmail.com>
2014-11-08 14:17:02 -0500

    Added method to ReaderWriter to combine data from projects and outcome files. Each of written files is 10000 full elements joined by projectID

--- project/src/ReaderWriter.scala
+++ project/src/ReaderWriter.scala
@@ -1,3 +1,4 @@
+import scala.io._
 
 /** Object to handle reading data from kaggle, assembling into Data objects */
 object ReaderWriter {
@@ -6,8 +7,60 @@ object ReaderWriter {
   val outcomesFile = "data/outcomes.csv"
   val projectsFile = "data/projects.csv"
   val resourcesFile = "data/resources.csv"
+  val combineFilePref = "data/combined_"
+    
   val outputFile = "data/SVMData.txt"
   
+  /** Alter to run whichever routine is necessary */
+  def main(args : Array[String]) : Unit = {
+    combineAndSplit(10000, 10)
+  }
+  
+  /** Reads from projects and outcomes files, then writes unioned data to new data files.
+   *  Assumes projects and outcomes files are both sorted by their projectID field.
+   *  Each file written to memory has n rows in it, plus the header row. Writes m files
+   */  
+  def combineAndSplit(n : Int, m : Int) : Unit = {
+    val projectsIterator = Source.fromFile(projectsFile).getLines
+    val outcomesIterator = Source.fromFile(outcomesFile).getLines
+
+    val projectsHeader = projectsIterator.next
+    val outcomesHeader = outcomesIterator.next
+    
+    //Unioned header that doesn't repeat the projectID field
+    val header = projectsHeader + "," + outcomesHeader.substring(outcomesHeader.indexOf(',') + 1)
+    
+    /** Writes the given list of Strings to the given filepath */
+    def write(elms : List[String], path : String) : Unit = {
+      val s = elms.foldLeft("")((acc, e) => acc + e + "\n")
+      val pw = new java.io.PrintWriter(new java.io.File(path))
+      try pw.write(s) 
+      finally pw.close()
+    }
+    
+    for(i <- (0 until m)){
+      println("Creating file " + (i+1))
+      var lst : List[(String, String)] = List()
+      while(lst.size < n){
+        
+        //Loop until we find a outcome and project with the same projectID.
+        //Assumes outcomes are a subset of projects.
+        
+        val outcome = outcomesIterator.next
+        var project = ""
+        do{
+          project = projectsIterator.next
+        }while(! outcome.substring(0, outcome.indexOf(',')).equals(project.substring(0, project.indexOf(','))))
+        
+        val outcomeWithoutID = outcome.substring(outcome.indexOf(',') + 1)  
+          
+        lst = (project, "," + outcomeWithoutID) :: lst  
+      }
+      
+      write(header :: lst.map(a => a._1 + a._2), combineFilePref + (i+1) + ".csv")
+    }
+  }  
+    
   /** Reads from the .csv files given, writes to a new file of svm data */
   def readThenWriteToSVMData() : Unit = {
     val donationsMap = read(donationsFile)
