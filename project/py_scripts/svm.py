23c3ac5a5e4a90c5dc3926a51538760dd0724d4e
Rachel <rae84@cornell.edu>
2014-11-10 18:10:10 -0500

    some simple debugging and add svm_light, this should be stable

--- project/py_scripts/svm.py
+++ project/py_scripts/svm.py
@@ -7,6 +7,8 @@ from sklearn.datasets import load_svmlight_file
 import tempfile
 import texttable
 
+w_out = open('../data/svm_out/w.out','w')
+w_out_sorted = open('../data/svm_out/w_sorted.out','w')
 # TODO better name for class
 class Instance():
     def __init__(self, train_dat_num, test_dat_num, c=None,j=None):
@@ -21,9 +23,8 @@ class Instance():
             prefix += '_c_' + str(c).replace('.','-')
         if j != None:
             prefix += '_j_' + str(j)
-        prefix += '.'
-        self.model = prefix + 'model'
-        self.prediction = prefix + 'prediction'
+        self.model = prefix + '.model'
+        self.prediction = prefix + '.prediction'
         self.classify_out = prefix +'_classify.out'
         self.kernel = None
         self.accuracy = None
@@ -46,21 +47,24 @@ class Kernel():
 
 
 def learn(instance):
-    args = ['./svm_learn']
+    args_list = ['./svm_light/svm_learn']
     if instance.c is not None:
-        args.append('-c')
-        args.append(str(instance.c))
+        args_list.append('-c')
+        args_list.append(str(instance.c))
     if instance.j is not None:
-        args.append('j')
-        args.append(str(instance.j))
-    args.append(instance.train_file)
-    args.append(instance.model)
-    popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+        args_list.append('j')
+        args_list.append(str(instance.j))
+    args_list.append(instance.train_file)
+    args_list.append(instance.model)
+    args = tuple(args_list)
+    f_tmp = tempfile.TemporaryFile()
+    popen = subprocess.Popen(args, stdout=f_tmp)
     popen.wait()
+    f_tmp.close()
 
 def classify(instance):
     output = open(instance.classify_out, 'w')
-    args = ('./svm_classify', instance.test_file, instance.model, instance.prediction)
+    args = ('./svm_light/svm_classify', instance.test_file, instance.model, instance.prediction)
     popen = subprocess.Popen(args, stdout=output)
     popen.wait()
     output.close()
@@ -73,6 +77,8 @@ def read_model(instance):
     instance.kernel = Kernel(*args)
     f_tmp = tempfile.TemporaryFile()
     f_tmp.writelines(lines[11:])
+    f_tmp.flush()
+    f_tmp.seek(0)
     x_data, y_data = load_svmlight_file(f_tmp)
     f_tmp.close()
     return x_data, y_data
@@ -84,6 +90,13 @@ def find_w(instance):
     x_mat = scipy.sparse.linalg.aslinearoperator(x_data)
     w = x_mat.matmat(y_data)
     instance.w = w
+    prt_str = str(instance.train_file) + "  " + str(w) + "\n"
+    w_out.write(prt_str)
+    w_out.flush() #TODO delete this (only for early testing)
+    sorted_w = sorted(range(len(w)), key=lambda x : w[x], reverse=True)
+    srtd_prt_str = str(instance.train_file) + "  " + str(sorted_w) + "\n"
+    w_out_sorted.write(srtd_prt_str)
+    w_out_sorted.flush()#TODO delete this (only for early testing)
 
 def find_results(instance):
     f = open(instance.prediction, 'r')
@@ -109,5 +122,8 @@ for i in DAT_NUMS[:-1]:
     instance = Instance(i,10)
     process(instance)
     table.add_row([instance.train_file, instance.accuracy, instance.fn, instance.fp])
+    print instance.train_file, instance.accuracy, instance.fn, instance.fp
 
+print "---------------------------"
+print
 print table.draw()
