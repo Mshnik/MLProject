1b95b51cb6c444fe7750c0c4120ad592bb852529
Rachel <rae84@cornell.edu>
2014-12-01 19:24:27 -0500

    add k option

--- project/py_scripts/svm.py
+++ project/py_scripts/svm.py
@@ -12,11 +12,12 @@ w_out = open('../data/svm_out/w.out','w')
 w_out_sorted = open('../data/svm_out/w_sorted.out','w')
 # TODO better name for class
 class Instance():
-    def __init__(self, train_dat_num, test_dat_num, c=None,j=None):
+    def __init__(self, train_dat_num, test_dat_num, c=None,j=None, k=None):
         self.train_data_num = train_dat_num
         self.test_data_num = test_dat_num
         self.c = c
         self.j = j
+        self.k = k
         self.train_file = '../data/svm_norm/dat_' + str(train_dat_num) +'.txt'
         self.test_file = '../data/svm_norm/dat_' + str(test_dat_num) +'.txt'
         prefix = '../data/svm_out/dat_' + str(train_dat_num)
@@ -56,6 +57,9 @@ def learn(instance):
     if instance.j is not None:
         args_list.append('-j')
         args_list.append(str(instance.j))
+    if instance.k is not None:
+        args_list.append('-k')
+        args_list.append(str(instance.k))
     args_list.append(instance.train_file)
     args_list.append(instance.model)
     args = tuple(args_list)
@@ -104,13 +108,13 @@ def find_results(instance):
     f = open(instance.prediction, 'r')
     _, y_data = load_svmlight_file(instance.test_file)
     predictions = [float(x.split()[0]) for x in f.readlines()]
-    instance.fn = sum([1. for (t, p) in zip(y_data, predictions) if t > 0 and p <= 0])
-    instance.fp = sum([1. for (t, p) in zip(y_data, predictions) if t <= 0 and p > 0])
-    instance.tp = sum([1. for (t, p) in zip(y_data, predictions) if t > 0 and p > 0])
+    instance.fn = sum([1 for (t, p) in zip(y_data, predictions) if t > 0 and p <= 0])
+    instance.fp = sum([1 for (t, p) in zip(y_data, predictions) if t <= 0 and p > 0])
+    instance.tp = sum([1 for (t, p) in zip(y_data, predictions) if t > 0 and p > 0])
     instance.accuracy = 1 - float(instance.fn + instance.fp)/len(y_data)
-    precision = instance.tp / (instance.tp+instance.fp)
-    recall = instance.tp / (instance.tp + instance.fn)
-    instance.f1 = 2 * precision * recall / (precision + recall)
+    precision = instance.tp / float(instance.tp+instance.fp)
+    recall = instance.tp / float(instance.tp + instance.fn)
+    instance.f1 = 2 * precision * recall / float(precision + recall)
     f.close()
 
 def process(instance):
@@ -119,13 +123,13 @@ def process(instance):
     find_results(instance)
     find_w(instance)
 
-def run(train_test_pairs, j_vals=[None], c_vals=[None]):
+def run(train_test_pairs, j_vals=[None], c_vals=[None], k=None):
     table = texttable.Texttable()
     table.header(['Train File', 'Test File', 'j', 'c',  '# FN ', '# FP', 'Accuracy','F1'])
     for (train, test) in train_test_pairs:
         for j in j_vals:
             for c in c_vals:
-                instance = Instance(train, test, c=c, j=j)
+                instance = Instance(train, test, c=c, j=j, k=k)
                 process(instance)
                 table.add_row([instance.train_file, instance.test_file, instance.j, instance.c, instance.fn, instance.fp, instance.accuracy,instance.f1])
                 print instance.train_file, instance.test_file, instance.j, instance.c, instance.fn, instance.fp, instance.accuracy, instance.f1
@@ -135,9 +139,9 @@ def run(train_test_pairs, j_vals=[None], c_vals=[None]):
 
 DAT_NUMS = range(1,11) #TODO zero index the data files
 
-J_VALS = [.6,.75,.8]
+J_VALS = [.75,1]
 C_VALS = [0.1,1.0,10.0]
 pairs = [(i,10) for i in DAT_NUMS[:-1]]
 
-run(pairs, j_vals=J_VALS)
+run(pairs,j_vals=J_VALS,k=2)
 
